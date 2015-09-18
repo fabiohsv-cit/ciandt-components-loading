8d0cd176be02c404814329f5d7d1a2f3380e1155
Henrique Silva Brighenti <henriqueb@ciandt.com>
2015-09-18 19:21:56 -0300

    Changes the implementation on checking the pending requests when closing the loadingModal
    Now uses a factory variable to keep track of the pending requests that need the modal blocking the screen and closes it as soon as they are over. Doesn't leave the job of closing the modal to the last request, instead it's done by the last request that actually opened the modal.

--- loading.js
+++ loading.js
@@ -1,19 +1,20 @@
 ﻿'use strict';
 
-define(['angular-animate', 'angular-loading-bar', 'ng-jedi-dialogs', 'ng-jedi-loading-directives'], function () {
+define(['angular-animate', 'angular-loading-bar', 'ng-jedi-dialogs', 'ng-jedi-loading-directives'], function() {
 
     angular.module('jedi.loading', ['ngAnimate', 'angular-loading-bar', 'jedi.loading.directives', 'jedi.dialogs']).constant('jedi.loading.LoadingConfig', {
         enableInfoAfterResponse: false,
         infoAfterResponseMessage: 'Operação realizada com sucesso.',
         enableLoadingBar: true,
         enableLoadingBlock: false,
-    }).factory('jedi.loading.LoadingInterceptor', ['$q', '$injector', '$timeout', 'jedi.loading.LoadingConfig', function ($q, $injector, $timeout, LoadingConfig) {
+    }).factory('jedi.loading.LoadingInterceptor', ['$q', '$injector', '$timeout', 'jedi.loading.LoadingConfig', function($q, $injector, $timeout, LoadingConfig) {
         var alertHelper;
+        var loadingModalCounter = 0;
         if (LoadingConfig.enableInfoAfterResponse) {
             alertHelper = $injector.get('jedi.dialogs.AlertHelper');
         }
         return {
-            request: function (config) {
+            request: function(config) {
                 var showLoadingModalDefined = angular.isDefined(config.showLoadingModal) || angular.isDefined(config.headers.showLoadingModal) || (angular.isDefined(config.params) && angular.isDefined(config.params.showLoadingModal));
                 var showLoadingModal = config.showLoadingModal || config.headers.showLoadingModal || (config.params && config.params.showLoadingModal);
 
@@ -23,42 +24,54 @@ define(['angular-animate', 'angular-loading-bar', 'ng-jedi-dialogs', 'ng-jedi-lo
 
                 if ((showLoadingModalDefined && showLoadingModal) || (!showLoadingModalDefined && LoadingConfig.enableLoadingBlock)) {
                     $('#loadingModal').modal('show');
+                    loadingModalCounter++;
+                    config.openLoadingModal = true;
                 }
 
                 return config;
             },
-            requestError: function (rejection) {
-                if ($injector.get('$http').pendingRequests < 1 && $('#loadingModal').hasClass('in')) {
-                    $('#loadingModal').modal('hide');
+            requestError: function(rejection) {
+                if (rejection.config.openLoadingModal) {
+                    loadingModalCounter--;
+                    if (loadingModalCounter === 0 && $('#loadingModal').hasClass('in')) {
+                        $('#loadingModal').modal('hide');
+                    }
                 }
+
                 return $q.reject(rejection);
             },
-            response: function (response) {
-                var showLoadingModal = response.config.showLoadingModal || response.config.headers.showLoadingModal || (response.config.params && response.config.params.showLoadingModal);
-                //Only hides loading modal when has no pending requests, the modal is open and the modal was open by this directive.
-                if ($injector.get('$http').pendingRequests < 1 && $('#loadingModal').hasClass('in') && showLoadingModal) {
-                    $('#loadingModal').modal('hide');
+            response: function(response) {
+                if (response.config.openLoadingModal) {
+                    loadingModalCounter--;
+                    if (loadingModalCounter === 0 && $('#loadingModal').hasClass('in')) {
+                        $('#loadingModal').modal('hide');
+                    }
                 }
+
+
                 if (LoadingConfig.enableInfoAfterResponse && (response.headers()['content-type'] && response.headers()['content-type'].toUpperCase().indexOf('JSON') >= 0) && (response.config.method.toUpperCase() != 'GET')) {
                     alertHelper.addInfo(LoadingConfig.infoAfterResponseMessage);
                 }
                 return response || $q.when(response);
             },
-            responseError: function (rejection) {
-                if ($injector.get('$http').pendingRequests < 1 && $('#loadingModal').hasClass('in')) {
-                    $('#loadingModal').modal('hide');
+            responseError: function(rejection) {
+                if (rejection.config.openLoadingModal) {
+                    loadingModalCounter--;
+                    if (loadingModalCounter === 0 && $('#loadingModal').hasClass('in')) {
+                        $('#loadingModal').modal('hide');
+                    }
                 }
+
                 return $q.reject(rejection);
             }
-        }
-    }]).config(['$httpProvider', function ($httpProvider) {
+        };
+    }]).config(['$httpProvider', function($httpProvider) {
         // configura interceptor para capturar erros de requisição http
         var $log = angular.injector(['ng']).get('$log');
         $log.info('Registrando mecanismo de loading.');
+
         // coloca este interceptor para ser executado primeiro, pra garantir que o loading-bar seja depois e o ignoreLoadingBar funcionar
-        var _0 = $httpProvider.interceptors[0];
-        $httpProvider.interceptors[0] = 'jedi.loading.LoadingInterceptor';
-        $httpProvider.interceptors.push(_0);
+        $httpProvider.interceptors.unshift('jedi.loading.LoadingInterceptor');
     }]);
 
 });
