3b2c44aba7b2f09814328490f44518b579189e6e
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-11-11 12:24:05 -0200

    add package.json, concat js and internalize html

--- loading.js
+++ loading.js
@@ -1,18 +1,56 @@
-﻿'use strict';
-
+/*
+ ng-jedi-loading v0.0.1
+ Loading component written in angularjs
+ https://github.com/jediproject/ng-jedi-loading
+*/
 (function (factory) {
     if (typeof define === 'function') {
-        define(['angular-animate', 'angular-loading-bar', 'ng-jedi-dialogs', 'ng-jedi-loading-directives'], factory);
+        define(['angular', 'angular-animate', 'angular-loading-bar', 'ng-jedi-dialogs'], factory);
     } else {
         if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){
             module.exports = 'jedi.loading';
             require('angular-loading-bar');
-            require('ng-jedi-loading-directives');
         }
         return factory();
     }
 }(function() {
+	"use strict";
 
+    angular.module("jedi.loading.directives", []).directive("jdLoading", [function () {
+        return {
+            restrict: 'E',
+            templateUrl: function (elem, attrs) {
+                if (attrs.templateUrl) {
+                    return attrs.templateUrl;
+                } else {
+                    return "assets/libs/ng-jedi-loading/loading.html";
+                }
+            }
+        };
+    }]).run(['$templateCache', function($templateCache) {
+        $templateCache.put('assets/libs/ng-jedi-loading/loading.html',  '<div class="modal" id="loadingModal" data-backdrop="static">'+
+                                                                        '    <div class="outer">'+
+                                                                        '        <div class="inner">'+
+                                                                        '            <div class="modal-dialog text-center">'+
+                                                                        '                <div class="sk-fading-circle">'+
+                                                                        '                    <div class="sk-circle1 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle2 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle3 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle4 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle5 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle6 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle7 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle8 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle9 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle10 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle11 sk-circle"></div>'+
+                                                                        '                    <div class="sk-circle12 sk-circle"></div>'+
+                                                                        '                </div>'+
+                                                                        '            </div>'+
+                                                                        '        </div>'+
+                                                                        '    </div>'+
+                                                                        '</div>');
+    }]);
     angular.module('jedi.loading', ['ngAnimate', 'angular-loading-bar', 'jedi.loading.directives', 'jedi.dialogs']).constant('jedi.loading.LoadingConfig', {
         enableInfoAfterResponse: false,
         infoAfterResponseMessage: 'Operação realizada com sucesso.',
@@ -59,7 +97,6 @@
                     }
                 }
 
-
                 if (LoadingConfig.enableInfoAfterResponse && (response.headers()['content-type'] && response.headers()['content-type'].toUpperCase().indexOf('JSON') >= 0) && (response.config.method.toUpperCase() !== 'GET')) {
                     alertHelper.addInfo(LoadingConfig.infoAfterResponseMessage);
                 }
@@ -79,7 +116,7 @@
     }]).config(['$httpProvider', function($httpProvider) {
         // configura interceptor para capturar erros de requisição http
         var $log = angular.injector(['ng']).get('$log');
-        $log.info('Registrando mecanismo de loading.');
+        $log.info('Configure jedi loading.');
 
         // coloca este interceptor para ser executado primeiro, pra garantir que o loading-bar seja depois e o ignoreLoadingBar funcionar
         $httpProvider.interceptors.unshift('jedi.loading.LoadingInterceptor');
